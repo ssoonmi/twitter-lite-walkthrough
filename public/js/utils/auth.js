96488c40d8f7b2ec0c24017fa5dc076f38dbe396
Soon-Mi Sugihara <ssoonmi@gmail.com>
2020-08-11 07:07:45 -0700

    Finish tweets index, create, show

--- public/js/utils/auth.js
+++ public/js/utils/auth.js
@@ -2,14 +2,12 @@ export const disableFormButton = (inputsQuery, buttonQuery) => {
   const inputs = document.querySelectorAll(inputsQuery);
 
   const button = document.querySelector(buttonQuery);
-
   const isAnyInputValueEmpty = () => {
     for (let input of inputs) {
       if (input.value === "") return true;
     }
     return false;
   };
-
   inputs.forEach((input) => {
     input.addEventListener("input", (e) => {
       if (!isAnyInputValueEmpty()) {
